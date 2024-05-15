f45d98ad10583fdf3f7a2f4af5e7f58c9235caf0
Ryan Lucena Silva <134936752+Ryanlssv@users.noreply.github.com>
2024-05-15 13:37:40 +0000

    Arquivo

---
+++ Aula 1/main.lua
@@ -0,0 +1,15 @@
+print("bom dia") -- Printa "bom dia" no console
+ bola = 232
+print(bola)
+hoje = "terça"
+print(hoje)
+io.write(hoje)
+
+print(a)
+a = "STRING" 
+b= 1 
+c = 2.5
+d = true
+e = false
+f = nil
+g = {}
